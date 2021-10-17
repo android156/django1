84b6e8a260258c52eb6882659d652667251f4af2
android156 <n_degtyarev@mail.ru>
2021-10-17 17:39:08 +0300

    rendering categories of product from db added

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,22 +1,18 @@
 from django.shortcuts import render
-
+from .models import ProductCategory, Product
 # Create your views here.
 
 
 def products(request):
 
     title = 'Каталог продукции'
-    links_menu = [
-        {'href': 'products_all', 'name': 'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'},
-    ]
+    categories = ProductCategory.objects.all()
+    products = Product.objects.all()[:4]
 
     context = {
         'title': title,
-        'links': links_menu
+        'products': products,
+        'categories': categories
     }
 
     return render(request, 'mainapp/products.html', context=context)
